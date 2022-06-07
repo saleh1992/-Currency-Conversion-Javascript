95e2034ca615eeabb8fda8e68276db059edbc26f
Saleh Abd Elmoanim <61623224+saleh1992@users.noreply.github.com>
2022-06-07 04:37:11 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,37 +1,8 @@
-let fontFamily = document.querySelector("#list-fontfamily");
-let fontColor = document.querySelector("#list-color");
-let fontSize = document.querySelector("#list-fontsize");
-let arrayContainer = [];
+let input = document.querySelector("input");
+let result = document.querySelector(".result");
+let egy = input.value * 18.5
 
-setDataInPage();
-
-document.addEventListener("input", () => {
-  result.style.fontFamily = fontFamily.value;
-  result.style.color = fontColor.value;
-  result.style.fontSize = fontSize.value + "px";
-  pushDatainArray();
-  setDataInStorge(arrayContainer);
-});
-
-function pushDatainArray() {
-  const dataValues = {
-    fontFamily: fontFamily.value,
-    color: fontColor.value,
-    fontSize: fontSize.value,
-  };
-  arrayContainer = dataValues;
-}
-function setDataInStorge(arr) {
-  localStorage.setItem("pagePr", JSON.stringify(arr));
-}
-function setDataInPage() {
-  if (localStorage.getItem("pagePr")) {
-    getAll = JSON.parse(localStorage.getItem("pagePr"));
-    fontFamily.value = getAll.fontFamily;
-    fontColor.value = getAll.color;
-    fontSize.value = getAll.fontSize;
-    result.style.fontFamily = getAll.fontFamily;
-    result.style.color = getAll.color;
-    result.style.fontSize = getAll.fontSize + "px";
-  }
-}
+input.addEventListener("input", () => {
+  let egy = input.value * 18.5
+    result.innerHTML = `{${input.value}} USD Dollar = {${egy.toFixed(2)}} Egyptian Pound`
+})
